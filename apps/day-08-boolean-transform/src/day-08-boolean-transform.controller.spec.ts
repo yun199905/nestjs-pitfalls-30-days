a14894ecab6faed03b898966bed394800ee8535b
cindy <Cindy.He@hepiuscare.com.tw>
2026-06-10 14:27:47 +0800

    feat(day-08): implement boolean transformation pitfalls

--- apps/day-08-boolean-transform/src/day-08-boolean-transform.controller.spec.ts
+++ apps/day-08-boolean-transform/src/day-08-boolean-transform.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Day08BooleanTransformController } from './day-08-boolean-transform.controller';
-import { Day08BooleanTransformService } from './day-08-boolean-transform.service';
 
 describe('Day08BooleanTransformController', () => {
   let day08BooleanTransformController: Day08BooleanTransformController;
@@ -8,15 +7,31 @@ describe('Day08BooleanTransformController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [Day08BooleanTransformController],
-      providers: [Day08BooleanTransformService],
     }).compile();
 
-    day08BooleanTransformController = app.get<Day08BooleanTransformController>(Day08BooleanTransformController);
+    day08BooleanTransformController =
+      app.get<Day08BooleanTransformController>(
+        Day08BooleanTransformController,
+      );
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(day08BooleanTransformController.getHello()).toBe('Hello World!');
+  describe('findPostsWithImplicitBoolean', () => {
+    it('should return transformed query directly', () => {
+      expect(
+        day08BooleanTransformController.findWithImplicitBoolean({
+          isPublished: true,
+        }),
+      ).toEqual({ isPublished: true });
+    });
+  });
+
+  describe('findPostsWithExplicitBoolean', () => {
+    it('should return transformed query directly', () => {
+      expect(
+        day08BooleanTransformController.findWithExplicitBoolean({
+          isPublished: false,
+        }),
+      ).toEqual({ isPublished: false });
     });
   });
 });
--- apps/day-08-boolean-transform/src/day-08-boolean-transform.module.ts
+++ apps/day-08-boolean-transform/src/day-08-boolean-transform.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { Day08BooleanTransformController } from './day-08-boolean-transform.controller';
-import { Day08BooleanTransformService } from './day-08-boolean-transform.service';
 
 @Module({
   imports: [],
   controllers: [Day08BooleanTransformController],
-  providers: [Day08BooleanTransformService],
 })
 export class Day08BooleanTransformModule {}
